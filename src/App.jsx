a2681ee7cd4a1fe41845b4d4ce7e16cbdced5258
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-10-20 20:13:19 +0500

    used useContext hook in different components

--- src/App.jsx
+++ src/App.jsx
@@ -28,12 +28,13 @@ function App() {
 
   return (
     <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn
+      isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler
     }}>
-      <MainHeader onLogout={logoutHandler} />
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {isLoggedIn && <Home />}
       </main>
     </AuthContext.Provider>
   );
